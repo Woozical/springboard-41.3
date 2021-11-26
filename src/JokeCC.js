f785ae72ee5a6d248e8cb2b56db14d1dc39fa057
Mark <mark@example.dev>
2021-11-25 21:27:48 -0800

    Joke Class Component

---
+++ src/JokeCC.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Joke.css";
+
+class Joke extends React.Component{
+  constructor(props){
+    super(props);
+    this.upVote = this.upVote.bind(this);
+    this.downVote = this.downVote.bind(this);
+  }
+
+  upVote(){
+    const { vote, id } = this.props;
+    vote(id, +1);
+  }
+
+  downVote(){
+    const { vote, id } = this.props;
+    vote(id, -1);
+  }
+
+  render(){
+    const {votes, text} = this.props;
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+  
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+  
+          {votes}
+        </div>
+  
+        <div className="Joke-text">{text}</div>
+      </div>
+    );
+  }
+}
+
+export default Joke
